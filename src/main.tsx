4c60aba68f453142b6e6fb3c551a6824a401c8e7
yannPGT <yannick.pagnot@icloud.com>
2025-06-09 18:07:04 +0200

    feat: add PWA support

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+  );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
